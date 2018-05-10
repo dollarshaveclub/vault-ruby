d202f3ab5a499b13724b2bc464f3d0be9d8bffc5
Eddy Kim <eddykim87@gmail.com>
2018-05-09 17:15:07 -0700

    removed debugging output

--- lib/vault/api/cacheable.rb
+++ lib/vault/api/cacheable.rb
@@ -2,10 +2,8 @@ module Vault
   module Cacheable
     def cache(url_path, cache_enabled)
       if cache_enabled && @cache[url_path]
-        puts "cache hit"
         @cache[url_path]
       else
-        puts "cache miss"
         @cache[url_path] = yield.dup
       end
     end
