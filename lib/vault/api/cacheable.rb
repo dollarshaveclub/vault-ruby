4e81c1da97072f87ad191b67d1abc81c43a31af1
Eddy Kim <eddykim87@gmail.com>
2018-05-16 14:45:39 -0700

    added cache debugging

--- lib/vault/api/cacheable.rb
+++ lib/vault/api/cacheable.rb
@@ -1,9 +1,12 @@
 module Vault
   module Cacheable
     def cache(url_path, cache_enabled)
+      puts "CACHE READ: #{url_path} CACHED_ENABLED? #{cache_enabled}"
       if cache_enabled && @cache[url_path]
+        puts "CACHE HIT: #{url_path} CACHED_ENABLED? #{cache_enabled}"
         @cache[url_path]
       else
+        puts "CACHE MISS: #{url_path} CACHED_ENABLED? #{cache_enabled}"
         @cache[url_path] = yield.dup
       end
     end
