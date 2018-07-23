fee3c07f1a6b8b957b8ae3bd449c46adb4f6d941
Alex Janckila <alex.janckila@dollarshaveclub.com>
2018-07-23 16:54:38 -0700

    k8s_auth_path first draft

--- lib/vault/defaults.rb
+++ lib/vault/defaults.rb
@@ -235,6 +235,13 @@ module Vault
       def ignore_connection_errors
         ENV["IGNORE_CONNECTION_ERRORS"] == 'true'
       end
+
+      # A default auth path option for using this gem with Kubernetes service accounts
+      # the value is added to the auth.kubernetes
+      def k8s_auth_path
+        ENV["VAULT_K8S_AUTH_PATH"] == 'kubernetes'
+      end
+      
     end
   end
 end
